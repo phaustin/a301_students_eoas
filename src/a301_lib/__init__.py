44bbe043d16682839e8739610bb0302225b0ec27
Phil Austin <paustin@eos.ubc.ca>
2023-01-12 20:25:19 -0800

    change shared_files

--- src/a301_lib/__init__.py
+++ src/a301_lib/__init__.py
@@ -2,12 +2,10 @@ from pathlib import Path
 import sys
 home_dir = Path().home()
 work_dir = home_dir / 'work'
-data_share = work_dir / 'data_share'
-a301_lib = work_dir / 'a301_lib'
-sat_data = work_dir / 'sat_data'
+data_share = home_dir / 'shared_files'
+sat_data = home_dir / 'sat_data'
 
-
-sys.path.insert(0, str(work_dir))
+sys.path.insert(0, str(home_dir))
 sep='*'*30
 print(f'{sep}\ncontext imported. Front of path:\n{sys.path[0]}\n')
 
