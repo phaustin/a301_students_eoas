f5bd563985a3be9f277dbf9ceaa26c8939773dcc
Phil Austin <paustin@eos.ubc.ca>
2023-01-12 20:29:40 -0800

    fix typo

--- src/a301_lib/a301_lib/__init__.py
+++ src/a301_lib/a301_lib/__init__.py
@@ -2,9 +2,9 @@ from pathlib import Path
 import sys
 home_dir = Path().home()
 work_dir = home_dir / 'work'
-data_share = work_dir / 'shared_files'
-sat_data = work_dir / 'sat_data'
+data_share = home_dir / 'shared_files'
+sat_data = home_dir / 'sat_data'
 
-sys.path.insert(0, str(work_dir))
+sys.path.insert(0, str(home_dir))
 sep='*'*30
 print(f'{sep}\ncontext imported. Front of path:\n{sys.path[0]}\n')
