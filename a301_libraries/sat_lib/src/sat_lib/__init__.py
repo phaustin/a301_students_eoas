f0c99230f303bfaa275aaef9c6acfb32a68c2717
Phil Austin <paustin@eos.ubc.ca>
2023-02-06 15:41:05 -0800

    version

--- a301_libraries/sat_lib/src/sat_lib/__init__.py
+++ a301_libraries/sat_lib/src/sat_lib/__init__.py
@@ -1,3 +1,16 @@
-print("in sat_lib init")
+from pathlib import Path
 from .modis_chans import modischan_dict
+print("in sat_lib init")
+root_dir = Path(__file__).resolve().parent
+version_file= root_dir / 'VERSION_satlib.txt'
+if not version_file.is_file():
+    __version__ = 'no_version'
+    try:
+        with open(version_file,'w') as f:
+            f.write(__version__)
+    except:
+        __version_file__=None
+else:
+    with open(version_file) as f:
+        __version__=f.read().strip()
 
--- a301_libraries/sat_lib/write_version.py
+++ a301_libraries/sat_lib/write_version.py
@@ -2,10 +2,10 @@ from setuptools_scm import get_version
 from pathlib import Path
 root_dir = Path().resolve()
 print(f"{root_dir=}")
-code_dir= root_dir / 'src/gradelib'
+code_dir= root_dir / 'src/sat_lib'
 git_version = get_version(root=str(root_dir))
 print(f"{git_version=}")
-version_file = code_dir / 'VERSION.txt'
+version_file = code_dir / 'VERSION_satlib.txt'
 with open(version_file,'w') as outfile:
     outfile.write(f"{git_version}\n")
 print((f"wrote new version number {git_version=}\n"
