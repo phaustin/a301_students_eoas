ddf880a9abdbd51f207776af3d4b28d77b92e91e
Phil Austin <paustin@eos.ubc.ca>
2023-02-23 11:56:21 -0800

    stac

--- notebooks/week7/stac_test.py
+++ notebooks/week7/stac_test.py
@@ -6,13 +6,6 @@ from pystac_client import Client
 from shapely.geometry import Point
 
 
-the_dir = Path() / "landsat_scenes"
-the_file = list(the_dir.glob("**/*2015*MTL.txt"))[0]
-the_meta = landsat_metadata(the_file)
-# for item in inspect.getmembers(the_meta):
-#     print(item)
-#     #print(f"{item}: {getattr(the_meta,item).default('NA')}")
-
 # connect to the STAC endpoint
 cmr_api_url = "https://cmr.earthdata.nasa.gov/stac/LPCLOUD"
 client = Client.open(cmr_api_url)
@@ -27,8 +20,35 @@ search = client.search(
 items = search.get_all_items()
 print(len(items))
 print(list(items))
+item = items[1]
+print(f"{item.datetime=}")
+print(f"{item.geometry=}")
+print(f"{item.properties}")
+assets = items[1].assets  # first item's asset dictionary
+print(assets.keys())
+for key, asset in assets.items():
+    print(f"{key}: {asset.title}")
 
+import rioxarray
 
+assets['browse'].href
 
+assets["B01"].href
 
+true_color_image = rioxarray.open_rasterio(assets["browse"].href) 
 
+ax = true_color_image.plot.imshow(figsize=(14,14),origin="upper");
+ax.axes.set_title("vancouver browse image");
+
+import a301_lib
+outfile = a301_lib.data_share / "pha/vancouver_browse.png"
+ax.write_png(outfile)
+
+import os
+os.environ["GDAL_HTTP_COOKIEFILE"] = "./cookies.txt"
+os.environ["GDAL_HTTP_COOKIEJAR"] = "./cookies.txt"
+
+b01_href = assets["B01"].href
+b01 = rioxarray.open_rasterio(b01_href)
+
+help(b01.rio.write_grid_mapping)
