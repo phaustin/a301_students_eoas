3b592936e2118ed8533779a7002b16e1b5acdac8
Phil Austin <paustin@eos.ubc.ca>
2023-03-08 12:16:02 -0800

    add landsat_read

---
+++ a301_libraries/sat_lib/src/sat_lib/landsat_read.py
@@ -0,0 +1,147 @@
+import copy
+import numpy as np
+from pystac_client import Client
+from shapely.geometry import Point
+from rasterio.windows import Window
+
+def get_clear_mask(fmask_ds):
+    """
+    return a DataArray copy of fmask_ds but with the pixel values set to
+    1 where there is both no cloud and pixel is land, and np.nan where there is cloud
+    or the pixel is water
+    
+    The bit patterns from the HSL QA mask are:
+     
+    Bits are listed from the MSB (bit 7) to the LSB (bit 0): 
+    7-6    aerosol:
+           00 - climatology
+           01 - low
+           10 - average
+           11 - high
+    5      water
+    4      snow/ice
+    3      cloud shadow
+    2      adjacent to cloud
+    1      cloud
+    0      cirrus cloud
+    
+    so a bit mask of 0b00100011 when anded with a QA value
+    will return non-zero when there is water, a cloud, or a cirrus cloud
+
+    Parameters
+    ----------
+
+    fmask_ds: xarray DataArray
+       landsat or sentinel hls scene create with rioxarray.open_rasterio
+
+    Returns
+    -------
+
+    clearmask_ds: xarray DataArray
+      new array with the clear sky mask for each pixel set to 1 if clear, np.nan if water or cloud
+    """
+    #
+    # convert float32 to unsigned 8 bit integer
+    #
+    bit_mask = fmask_ds.data.astype(np.uint8)
+    ref_mask = np.zeros_like(bit_mask)
+    #
+    # don't destroy original fmask DataArray
+    #
+    clearmask_ds = copy.deepcopy(fmask_ds)
+    #
+    # work with unsigned 8 bit values instead of
+    # base 10 floats
+    #
+    bit_mask = fmask_ds.data.astype(np.uint8)
+    #
+    # create a reference mask that will select
+    # bits 5, 1 and 0, which we want tocheck
+    #
+    ref_mask = np.zeros_like(bit_mask)
+    ref_mask[...] = 0b00100011  #find water (bit 5), cloud (bit 1) , cirrus (bit 0)
+    #
+    # if all three of those bits are 0, then bitwise_and will return 0
+    # otherwise it will return either a value greater than 0
+    #
+    cloudy_values = np.bitwise_and(bit_mask,ref_mask)
+    cloudy_values[cloudy_values>0]=1  #cloud or water
+    cloudy_values[cloudy_values==0]=0 #rest of scene
+    #
+    # now invert this, writing np.nan where there is 
+    # cloud or water.  Go back to float32 so we can use np.nan
+    #
+    clear_mask = cloudy_values.astype(np.float32)
+    clear_mask[cloudy_values == 1]=np.nan
+    clear_mask[cloudy_values == 0]=1
+    clearmask_ds.data = clear_mask
+    return clearmask_ds
+
+
+def get_landsat_scene(date, lon, lat, window):
+    """
+    retrieve windowed band4, band5 and Fmask for a given day and save
+    the clipped geotiffs into three DataArrays, returned
+    in a dictionary
+    
+    Parameters
+    ----------
+    
+    date: str
+       date in the form yyy-mm-yy
+    lon: float
+       longitude of point in the scene (degrees E)
+    lat: 
+        latitude of point in the scene (degrees N)
+    window: rasterio.Window
+        window for clipping the scene to a subscene
+  
+    Returns
+    -------
+    out_dict: dict
+       dictionary with three xarray DataArraysband4, band5, Fmask: rioxarrays with the data
+    """
+    #
+    # set up the search -- we are looking for only 1 scene per date
+    #
+    the_point = Point(lon, lat)
+    cmr_api_url = "https://cmr.earthdata.nasa.gov/stac/LPCLOUD"
+    client = Client.open(cmr_api_url)
+    
+    search = client.search(
+        collections=["HLSL30.v2.0"],
+        intersects=the_point,
+        datetime= date
+    )
+    items = search.get_all_items()
+    print(f"found {len(items)} item")
+    #
+    # get the metadata and add date, cloud_cover and band_name to the new DataArrays
+    #
+    props = items[0].properties
+    out_dict = {}
+    chan_names = ['B04','B05','Fmask']
+    array_names = ['b4_ds','b5_ds','fmask_ds']
+    for chan,array_name in zip(chan_names, array_names):
+        print(f"inside get_landsat_scene: reading {chan} into {array_name}")
+        href = items[0].assets[chan].href
+        lazy_ds = rioxarray.open_rasterio(href,mask_and_scale=True)
+        #
+        # now read the window
+        #
+        clipped_ds = lazy_ds.rio.isel_window(window)
+        #
+        # add some custom attributes
+        #
+        clipped_ds.attrs['date'] = props['datetime'] #date and time
+        clipped_ds.attrs['cloud_cover'] = props['eo:cloud_cover']
+        clipped_ds.attrs['band_name'] = chan
+        utm_zone = clipped_ds.attrs['HORIZONTAL_CS_NAME'][-3:].strip()
+        clipped_ds.attrs['cartopy_epsg_code'] = find_epsg_code(utm_zone)
+        clipped_ds.attrs['day']=props['datetime'][:10]  #yyyy-mm-dd
+        out_dict[array_name] = clipped_ds
+    #
+    # convert the mask to 1=no cloud over land, np.nan=otherwise
+    #
+    out_dict['fmask_ds'] = get_clear_mask(out_dict['fmask_ds'])
+    return out_dict
