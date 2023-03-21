43ec216c16be5fdc25d5346952a0f0c6ff9bfcdb
Phil Austin <paustin@eos.ubc.ca>
2023-03-21 13:40:02 -0700

    new hdf4 verbose

--- a301_libraries/sat_lib/src/sat_lib/hdf4_inspect.py
+++ a301_libraries/sat_lib/src/sat_lib/hdf4_inspect.py
@@ -1,16 +1,21 @@
 import click
+from pathlib import Path
+from importlib.metadata import version, PackageNotFoundError
 from pyhdf.HDF import *
 from pyhdf.V   import *
 from pyhdf.VS  import *
 from pyhdf.SD  import *
-from importlib.metadata import version, PackageNotFoundError
 from .modischan_read import sd_open_file
-from pathlib import Path
+from .cloudsat import read_swath_attributes
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
 
 try:
     __version__ = version("sat_lib")
 except PackageNotFoundError:
     __version__ = "unknown version"
+
 
 def format_line(line_dict):
     if 'dims' in line_dict:
@@ -22,8 +27,9 @@ def format_line(line_dict):
 
 @click.command()
 @click.version_option(__version__)
+@click.option('--verbose', '-v', is_flag=True, help="Print all attributes")
 @click.argument('hdf_file',type=str,nargs=1)
-def main(hdf_file):
+def main(hdf_file,verbose):
     """
     print information about an hdf4 file
 
@@ -37,12 +43,15 @@ def main(hdf_file):
 
     side effect: printing the hdf_file info
     """
-    hdfname = Path(hdf_file).resolve()
-    hdfname=str(hdfname)
+    hdf_path = Path(hdf_file).resolve()
+    if not hdf_path.is_file():
+        raise ValueError("could not fine {hdf_path}")
+    hdfname=str(hdf_path)
     sd = sd_open_file(hdfname)
     hdf = HDF(hdfname, HC.READ)
     vs = hdf.vstart()
     v  = hdf.vgstart()
+    attr_dict = read_swath_attributes(v,vs)
     ref = v.find('Data Fields')
     vg = v.attach(ref)
     members = vg.tagrefs()
@@ -73,4 +82,10 @@ def main(hdf_file):
         value = var_dict[key]
         dims = format_line(value)
         print(f"{key}: {dims}")
-
+    if verbose:
+        print("\nAttributes\n")
+        pp.pprint(attr_dict)
+    v.end()
+    vs.end()
+    sd.end()
+    hdf.close()
